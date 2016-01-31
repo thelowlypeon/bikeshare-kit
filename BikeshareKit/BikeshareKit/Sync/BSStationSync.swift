396228b05dded4fd0d81a76f9686724e799d7a54
thelowlypeon <peter@onedesigncompany.com>
2016-01-31 09:35:53 -0600

    fixes typo

--- BikeshareKit/BikeshareKit/Sync/BSStationSync.swift
+++ BikeshareKit/BikeshareKit/Sync/BSStationSync.swift
@@ -88,7 +88,7 @@ extension BSService {
             var message = "Invalid station data: \(JSON)"
             if let dict = JSON as? NSDictionary {
                 if let err = dict["error"] as? String {
-                    message = "Error sync station data: \(err)"
+                    message = "Error syncing station data: \(err)"
                 }
             }
             return NSError(domain: "com.outofsomething",
